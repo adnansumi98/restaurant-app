1c44a1319ff4c8d51c7d8b4cbb2dff2482cef8e2
S. Mohamed Adnan <62370865+adnansumi98@users.noreply.github.com>
2024-07-05 01:12:02 +0530

    included react spinner

--- src/components/FoodItems/index.jsx
+++ src/components/FoodItems/index.jsx
@@ -1,9 +1,10 @@
 import { useEffect, useState } from 'react'
+import { ScaleLoader } from 'react-spinners'
 import './index.css'
 
 const FoodItems = () => {
   const [foodItems, setFoodItems] = useState([])
-  const [isLoading, setIsLoading] = useState(false)
+  const [isLoading, setIsLoading] = useState(true)
   const [quantity, setQuantity] = useState(0)
 
   const fetchFoodItems = async () => {
@@ -13,53 +14,68 @@ const FoodItems = () => {
     }
     await fetch(url, options)
       .then((response) => response.json())
-      .then((isLoading) => setIsLoading(true))
+      .then(setIsLoading(true))
       .then((data) => setFoodItems(data))
+      .catch((error) => console.log(error))
   }
 
   useEffect(() => {
     fetchFoodItems()
   }, [])
 
-  const decreaseQuantity = () => {
-    if (quantity > 0) {
-      setQuantity((prevQuantity) => prevQuantity - 1)
-    }
-  }
-
-  const increaseQuantity = () => {
-    if (quantity < 20) {
-      setQuantity((prevQuantity) => prevQuantity + 1)
-    }
-  }
-
   return (
-    <div className="fooditems-container">
-      <img className="food-mark" src="" alt="" />
-      <div className="food-details-container">
-        <h1 className="food-name">{'name'}</h1>
-        <p className="food-price">{'price'}</p>
-        <p className="food-description">{'description'}</p>
-        <div className="quantity-container">
-          <button
-            type="button"
-            className="quantity-button"
-            onClick={decreaseQuantity}
-          >
-            -
-          </button>
-          <p className="food-quantity">{quantity}</p>
-          <button
-            className="quantity-button"
-            type="button"
-            onClick={increaseQuantity}
-          >
-            +
-          </button>
+    <div>
+      {isLoading ? (
+        <div className="spinner-container ">
+          <ScaleLoader />
         </div>
-      </div>
-      <p className="food-calories">{'calories'}</p>
-      <img className="food-images" src="" alt="" />
+      ) : (
+        <ul className="fooditems-container">
+          {foodItems.map((foodItem) => {
+            return (
+              <li key={foodItem.id}>
+                <img
+                  className="food-mark"
+                  src={foodItem.markImageUrl}
+                  alt="mark"
+                />
+                <div className="food-details-container">
+                  <h1 className="food-name">{foodItem.name}</h1>
+                  <p className="food-price">{foodItem.price}</p>
+                  <p className="food-description">{foodItem.description}</p>
+                  <div className="quantity-container">
+                    <button
+                      type="button"
+                      className="quantity-button"
+                      onClick={() =>
+                        setQuantity((count) => (count > 0 ? count - 1 : count))
+                      }
+                    >
+                      -
+                    </button>
+                    <p className="food-quantity">{quantity}</p>
+                    <button
+                      className="quantity-button"
+                      type="button"
+                      onClick={() =>
+                        setQuantity((count) => (count < 20 ? count + 1 : count))
+                      }
+                    >
+                      +
+                    </button>
+                  </div>
+                </div>
+                <p className="food-calories">{foodItem.calories}</p>
+                <img
+                  className="food-images"
+                  src={foodItem.imageUrl}
+                  alt="food-image"
+                />
+              </li>
+            )
+          })}
+        </ul>
+      )}
     </div>
   )
 }
